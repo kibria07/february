a7ba6287b54fa3d6c349d6b5707f0aebe58a0af0
kibria <kibria07@gmail.com>
2015-04-14 22:48:39 -0400

    2nd february modify

--- src/dimentionalArray/ArrLstToArray.java
+++ src/dimentionalArray/ArrLstToArray.java
@@ -17,8 +17,11 @@ public class ArrLstToArray {
 		Integer arr[] = new Integer[arrList.size()];
 		arr=arrList.toArray(arr);
 		int count=0;
+		System.out.println(count);
 		for(int i : arr) count +=1;
 		System.out.println("count is " + count);
+		
+		
 		for(int j=0; j < arr.length;j++){
 			System.out.println("sjth elemens is " + j);
 		}
